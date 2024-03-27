721ce137f3605b145a45ad5d497911c649d3dfd8
tabby8612 <tabishsajwani@hotmail.com>
2024-03-27 14:30:43 +0400

    Application Added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Coming-Soon-Page-JS
+
+
+This is simple launching soon page.
+
+Here's the screenshot:
+
+![Screenshot](images/background.png)
---
+++ script.js
@@ -0,0 +1,26 @@
+const countDownDate = new Date("27 April 2024 00:00:00").getTime();
+
+
+const x = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+
+    }
+}, 1000);
